3a0b39ac50f19854a0512a05c9f948e98e51b3d3
杨诗楠 <yangsn@figo.cn>
2017-05-11 18:12:03 +0800

    去掉短信过期

--- sites/meilibo/api/OpenAPI/Lib/Action/V1/SMSAction.class.php
+++ sites/meilibo/api/OpenAPI/Lib/Action/V1/SMSAction.class.php
@@ -172,7 +172,7 @@ class SMSAction extends BaseAction
         $code = $this->generateRandomStr($this->captcha_length);
         $expired_time = 5; // 单位分钟.
         $msg = array("mobile"=> $phone, "message"=>L('_SMS_MESSAGE_ONE_').$code.L('_SMS_MESSAGE_TOW_')."【喵榜直播】");
-        $this->mmc->set('verify_code_'.$phone, $code, $expired_time * 60);
+        $this->mmc->set('verify_code_'.$phone, $code/*, $expired_time * 60*/);
         try {
 
 
