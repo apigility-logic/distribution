e2eeb55d855bcd49a2addd755220b9aabab8b941
杨诗楠 <yangsn@figo.cn>
2017-05-26 17:46:53 +0800

    初步完成数据结构定义

--- src/DoctrineEntity/Event.php
+++ src/DoctrineEntity/Event.php
@@ -3,28 +3,29 @@
  * Created by PhpStorm.
  * User: figo-007
  * Date: 2017/5/26
- * Time: 15:10:31
+ * Time: 16:01:52
  */
 namespace ApigilityLogic\Distribution\DoctrineEntity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\OneToOne;
 use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\JoinTable;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\ManyToMany;
 use Doctrine\ORM\Mapping\OneToMany;
-use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * Class ChainLevel
+ * Class Event
  * @package ApigilityLogic\Distribution\DoctrineEntity
- * @Entity @Table(name="al_dist_chain_level")
+ * @Entity @Table(name="al_dist_event")
  */
-class ChainLevel
+class Event
 {
     /**
      * @Id @Column(type="integer")
@@ -33,50 +34,43 @@ class ChainLevel
     protected $id;
 
     /**
-     * 链级
+     * 交易事件的发生额
      *
-     * @Column(type="integer", length=50, nullable=false)
+     * @Column(type="decimal", precision=11, scale=2, nullable=false)
      */
-    protected $level;
+    protected $amount;
 
     /**
-     * 分佣比值
+     * 基准比值（当前记录的）
      *
      * @Column(type="decimal", precision=5, scale=2, nullable=false)
      */
-    protected $percent;
+    protected $base_percent;
 
     /**
-     * 此链级关聊的所有分佣项
+     * 创建时间
+     *
+     * @Column(type="datetime", nullable=false)
+     */
+    protected $create_time;
+
+    /**
+     * 分佣事件发生的分销链节点
+     *
+     * @ManyToOne(targetEntity="Distributor", inversedBy="events")
+     * @JoinColumn(name="distributor_id", referencedColumnName="id")
+     */
+    protected $distributor;
+
+    /**
+     * 事件产生的所有分佣项
      *
      * @OneToMany(targetEntity="Commission", mappedBy="event")
      */
     protected $commissions;
 
     public function __construct() {
+        $this->downstream_distributors = new ArrayCollection();
         $this->commissions = new ArrayCollection();
     }
-
-    public function setId($id)
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function setPercent($percent)
-    {
-        $this->percent = $percent;
-        return $this;
-    }
-
-    public function getPercent()
-    {
-        return $this->percent;
-    }
 }
-
