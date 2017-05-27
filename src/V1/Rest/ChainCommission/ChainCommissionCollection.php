45fdcd4be96adecf3c01dc29af87a7b87fed7bee
杨诗楠 <yangsn@figo.cn>
2017-05-27 10:22:09 +0800

    配置所有现有doctrine实体暴露为rest接口

---
+++ src/V1/Rest/ChainCommission/ChainCommissionCollection.php
@@ -0,0 +1,8 @@
+<?php
+namespace ApigilityLogic\Distribution\V1\Rest\ChainCommission;
+
+use Zend\Paginator\Paginator;
+
+class ChainCommissionCollection extends Paginator
+{
+}
---
+++ src/V1/Rest/Event/EventResource.php
@@ -0,0 +1,8 @@
+<?php
+namespace ApigilityLogic\Distribution\V1\Rest\Event;
+
+use ZF\Apigility\Doctrine\Server\Resource\DoctrineResource;
+
+class EventResource extends DoctrineResource
+{
+}
